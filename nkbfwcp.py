86bc6a56e4799b567075c82e5d76905f27d1f65d
nut1414 <nut.pannapat@gmail.com>
2020-05-01 21:52:18 +0700

    Device Check

--- nkbfwcp.py
+++ nkbfwcp.py
@@ -1,28 +1,53 @@
 from tkinter import *
 import tkinter.messagebox
 import nkbser 
+import sys
 
 root = Tk()
-root.title('nkbfwcp')
-root.geometry("500x500")
-class deviceChooser:
-    deviceroot = nkbser.deviceList()
+
+
+
+deviceroot = nkbser.deviceList()
+class deviceChooser(Frame):
+    def __init__(self,parent):
+        self.parent = parent
+        parent.title('nkbfwcp')
+        parent.geometry("225x255")
+        parent.resizable(0,0)
+        parent.iconbitmap('f.ico')
+        print(deviceroot.deviceCount)
+        self.devicestring = StringVar(parent)
+        self.devText(self.devicestring,deviceroot.deviceCount)
+        self.Introtext = Label(parent,text="Choose your device")
+        self.devicetext = Label(parent,textvariable=self.devicestring,underline=9)
+        self.devicepick = Listbox(parent,width=35)
+        self.okbutton = Button(parent,text="OK",padx=15,command=sys.exit)
+        self.updatebutton = Button(parent,text="Refresh",padx=15,command=lambda : self.updateList(self.devicepick))
+        self.updateList(self.devicepick)
+        self.Introtext.pack(side=TOP)
+        self.devicetext.pack(side=TOP)
+        self.devicepick.pack()
+        self.okbutton.pack(fill=X,side=BOTTOM)
+        self.updatebutton.pack(fill=X,side=BOTTOM)
+
+    def updateList(self,sellist):
+        print(deviceroot.deviceCount)
+        deviceroot.__init__()
+        self.devText(self.devicestring,deviceroot.deviceCount)
+        sellist.delete(0,1)
+        for i in range(0,deviceroot.deviceCount):
+            if "Arduino" in deviceroot.manufacturer[i]:
+                inserttext = "%s <- Most Likely" % deviceroot.device[i]
+            else:
+                inserttext = deviceroot.device[i]
+            sellist.insert(i,inserttext)
+    def devText(self,variable,number):
+        variable.set("There is %d device(s) connected." % number)
+        
     
-    variable = StringVar(root)
-    print(deviceroot.deviceCount)
-
-    devicestring = "There is %d device(s) connected." % deviceroot.deviceCount
-
-    devicetext = Label(root,text=devicestring)
-    devicpick = OptionMenu(root,variable,*deviceroot.list)
-    devicebutton = Button(root,text="OK")
-    devicetext.pack()
-    devicpick.pack()
-    devicebutton.pack()
 
 
 
 
-
-windevchoose = deviceChooser
+windevchoose = deviceChooser(root)
 root.mainloop()
