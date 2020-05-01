0270651c646222cdeeba2d10ba2eaa213c0b48a0
nut1414 <nut.pannapat@gmail.com>
2020-05-01 19:52:00 +0700

    Update nkbfwcp.py

--- nkbfwcp.py
+++ nkbfwcp.py
@@ -3,18 +3,26 @@ import tkinter.messagebox
 import nkbser 
 
 root = Tk()
-deviceroot = nkbser.deviceList()
 root.title('nkbfwcp')
 root.geometry("500x500")
-variable = StringVar(root)
-print(deviceroot.deviceCount)
+class deviceChooser:
+    deviceroot = nkbser.deviceList()
+    
+    variable = StringVar(root)
+    print(deviceroot.deviceCount)
 
-devicestring = "There is %d device(s) connected." % deviceroot.deviceCount
+    devicestring = "There is %d device(s) connected." % deviceroot.deviceCount
 
-devicetext = Label(root,text=devicestring)
-devicpick = OptionMenu(root,variable,*deviceroot.list)
-devicebutton = Button(root,text="OK")
-devicetext.pack()
-devicpick.pack()
-devicebutton.pack()
+    devicetext = Label(root,text=devicestring)
+    devicpick = OptionMenu(root,variable,*deviceroot.list)
+    devicebutton = Button(root,text="OK")
+    devicetext.pack()
+    devicpick.pack()
+    devicebutton.pack()
+
+
+
+
+
+windevchoose = deviceChooser
 root.mainloop()
